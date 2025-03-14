7e8527c775e9d04fd5e617a7dde74b2c74642635
valetedd <vale98h@gmail.com>
2025-03-14 17:22:03 +0100

    saving prodLDA model before experimenting

--- prodLDA_model.py
+++ prodLDA_model.py
@@ -21,14 +21,14 @@ class HiddenToLogNormal(nn.Module):
     def __init__(self, hidden_size, num_topics):
         super().__init__()
         self.fcmu = nn.Linear(hidden_size, num_topics)
-        self.bnmu = nn.BatchNorm1d(num_topics)
+        # self.bnmu = nn.BatchNorm1d(num_topics)
         
         self.fclv = nn.Linear(hidden_size, num_topics)
-        self.bnlv = nn.BatchNorm1d(num_topics)
+        # self.bnlv = nn.BatchNorm1d(num_topics)
 
     def forward(self, hidden):
-        mu = self.bnmu(self.fcmu(hidden))
-        lv = self.bnlv(self.fclv(hidden))
+        mu = self.fcmu(hidden)
+        lv = self.fclv(hidden)
         dist = LogNormal(mu, (0.5 * lv).exp())
         return dist
 
@@ -37,10 +37,10 @@ class HiddenToDirichlet(nn.Module):
     def __init__(self, hidden_size, num_topics):
         super().__init__()
         self.fc = nn.Linear(hidden_size, num_topics)
-        self.bn = nn.BatchNorm1d(num_topics)
+        # self.bn = nn.BatchNorm1d(num_topics)
 
     def forward(self, hidden):
-        alphas = F.softplus(self.fc(hidden)) + 0.1
+        alphas = F.softplus(self.fc(hidden)) + 0.1 # constant to ensure positivity
         dist = Dirichlet(alphas)
         return dist
 
@@ -49,26 +49,38 @@ class Decoder(nn.Module):
     def __init__(self, vocab_size, num_topics, dropout):
         super().__init__()
         self.fc = nn.Linear(num_topics, vocab_size)
-        self.bn = nn.BatchNorm1d(vocab_size)
+        # self.bn = nn.BatchNorm1d(vocab_size)
         self.drop = nn.Dropout(dropout) 
 
     def forward(self, inputs):
         inputs = self.drop(inputs)
-        return F.log_softmax(self.bn(self.fc(inputs)), dim=1)
+        return F.log_softmax(self.fc(inputs), dim=1)
         # logarithmic probabilities align better to NLL loss (reconstruction loss)
 
 
 class ProdLDA(nn.Module):
-    def __init__(self, vocab_size, hidden_size, num_topics,
-                 dropout, use_lognormal=False):
+    def __init__(self, 
+                 vocab_size, 
+                 hidden_size, 
+                 num_topics,
+                 dropout, 
+                 use_lognormal=False):
         super().__init__()
+        
         self.encode = Encoder(vocab_size, hidden_size, dropout)
         if use_lognormal:
             self.h2t = HiddenToLogNormal(hidden_size, num_topics)
         else:
             self.h2t = HiddenToDirichlet(hidden_size, num_topics)
         self.decode = Decoder(vocab_size, num_topics, dropout)
-    
+        self.apply(self.weights_init)
+
+    def weights_init(self, m):
+        if isinstance(m, nn.Linear):
+            nn.init.xavier_uniform_(m.weight)
+            if m.bias is not None:
+                nn.init.zeros_(m.bias)
+                
     def forward(self, inputs):
         h = self.encode(inputs)
         posterior = self.h2t(h)
